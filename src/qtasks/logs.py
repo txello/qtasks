d1bbe2f317bca3ac6891d7d86208d77a4cdd0e80
txello <xelloworld@gmail.com>
2025-05-23 12:07:50 +0500

    feat(logging): Updated Logger class with annotations and better documentation; added new documentation file for logging

--- src/qtasks/logs.py
+++ src/qtasks/logs.py
@@ -1,13 +1,73 @@
 import logging
+from typing_extensions import Annotated, Doc
 
 
 class Logger:
+    """
+    `Logger` - Класс логирования, используется всеми компонентами.
+
+    ## Example
+
+    ```python
+    from qtasks import QueueTasks
+    from qtasks.logs import Logger
+    
+    logger = Logger(name="QueueTasks", subname="Global")
+    app = QueueTasks(logs=logger)
+
+    app.log.debug("Тест") # asctime [QueueTasks: DEBUG] (QueueTasks) Тест
+    ```
+    """
+
     def __init__(self,
-            name: str,
-            subname: str = None,
-            default_level: int = logging.DEBUG,
-            format: str = None
+            name: Annotated[
+                str,
+                Doc(
+                    """
+                    Имя. Используется в шаблоне `%(name)s`.
+                    """
+                )
+            ],
+
+            subname: Annotated[
+                str,
+                Doc(
+                    """
+                    Имя компонента.
+
+                    По умолчанию: None.
+                    """
+                )
+            ] = None,
+            default_level: Annotated[
+                str,
+                Doc(
+                    """
+                    Level по умолчанию.
+                    
+                    По умолчанию: `logging.DEBUG`.
+                    """
+                )
+            ] = logging.DEBUG,
+            format: Annotated[
+                str,
+                Doc(
+                    """
+                    Формат логирования.
+                    
+                    По умолчанию: `%(asctime)s [%(name)s: %(levelname)s] (%(subname)s) %(message)s`.
+                    """
+                )
+            ] = None
         ):
+        """Экземпляр Logger.
+
+        Args:
+            name (str): Имя. Используется в шаблоне `%(name)s`.
+            subname (str, optional): Имя компонента. По умолчанию: None.
+            default_level (int, optional): Level по умолчанию. По умолчанию: `logging.DEBUG`.
+            format (str, optional): Формат логирования. По умолчанию: `%(asctime)s [%(name)s: %(levelname)s] (%(subname)s) %(message)s`.
+        """
         self.name = name
         self.subname = subname
         logging.basicConfig(
@@ -16,11 +76,21 @@ class Logger:
         )
         self.logger = logging.getLogger(name)
     
-    def critical(self, *args, **kwargs): self._log(logging.CRITICAL, *args, **kwargs)
-    def error(self, *args, **kwargs): self._log(logging.ERROR, *args, **kwargs)
-    def warning(self, *args, **kwargs): self._log(logging.WARNING, *args, **kwargs)
-    def info(self, *args, **kwargs): self._log(logging.INFO, *args, **kwargs)
-    def debug(self, *args, **kwargs): self._log(logging.DEBUG, *args, **kwargs)
+    def critical(self, *args, **kwargs):
+        """Critical"""
+        self._log(logging.CRITICAL, *args, **kwargs)
+    def error(self, *args, **kwargs):
+        """Error"""
+        self._log(logging.ERROR, *args, **kwargs)
+    def warning(self, *args, **kwargs):
+        """Warning"""
+        self._log(logging.WARNING, *args, **kwargs)
+    def info(self, *args, **kwargs):
+        """Info"""
+        self._log(logging.INFO, *args, **kwargs)
+    def debug(self, *args, **kwargs):
+        """Debug"""
+        self._log(logging.DEBUG, *args, **kwargs)
     
     def _log(self, level, msg, *args, **kwargs):
         extra = kwargs.pop("extra", {})
@@ -28,4 +98,12 @@ class Logger:
         self.logger.log(level, msg, *args, extra=extra, **kwargs)
     
     def with_subname(self, new_subname: str) -> "Logger":
+        """Обновляем `subname`.
+
+        Args:
+            new_subname (str): Новый `subname`.
+
+        Returns:
+            Logger: Новый `Logger`.
+        """
         return Logger(self.name, new_subname, default_level=self.logger.level)
